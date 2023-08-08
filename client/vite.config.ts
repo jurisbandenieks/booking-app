b32de2737be4e14ff22cd4d53ad39ffd117da380
Juris <jurisbandenieks90@gmail.com>
2023-08-07 21:59:22 -0700

    containers

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+
+const PORT = 3000;
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: "0.0.0.0",
+    port: PORT,
+    watch: {
+      usePolling: true
+    }
+  },
+  base: "/client"
+});
