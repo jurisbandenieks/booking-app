a0481cbc5ef9bc07addf465e8a87142c723966d7
Juris <jurisbandenieks90@gmail.com>
2023-08-29 21:05:19 -0700

    deps removed

--- client/src/routes.tsx
+++ client/src/routes.tsx
@@ -1,12 +1,8 @@
-import { Navigate, useLocation, useRoutes } from "react-router-dom";
+import { useRoutes } from "react-router-dom";
 import { Dashboard, Login, NotFound } from "./views";
 import { LayoutWithAuth } from "./components";
-import { useAuth0 } from "@auth0/auth0-react";
 
 export const Routing = () => {
-  const { isAuthenticated } = useAuth0();
-  const location = useLocation();
-
   return useRoutes([
     {
       path: "/",
