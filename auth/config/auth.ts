19503a212089985104589bc238b60cb832aa59b4
Juris Bandenieks <36843793+jurisbandenieks@users.noreply.github.com>
2026-02-04 11:22:39 -0800

    Remove secret key from auth configuration
    Removed the secret key from the configuration.

--- auth/config/auth.ts
+++ auth/config/auth.ts
@@ -4,5 +4,5 @@ export const config = {
   baseURL: "http://localhost:3050",
   clientID: "lWLaeI75AzZOTAMyrmLozBq1pAHpT0jH",
   issuerBaseURL: "https://dev-rxfrw6obup4z0hez.us.auth0.com",
-  secret: "c87d8f84a4347a32e8d44b6f3b187c074e943028d40ad275fed72cda3429369c"
+  secret: ""
 };
